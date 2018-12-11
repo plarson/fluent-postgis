d1c888b33b3c5a24885bb000590b4591f2fa1733
Phil Larson <phil.larson@icloud.com>
2018-12-11 11:17:19 -0800

    Add more data to test

--- Tests/FluentPostGISTests/FluentPostGISTests.swift
+++ Tests/FluentPostGISTests/FluentPostGISTests.swift
@@ -98,7 +98,12 @@ final class FluentPostGISTests: XCTestCase {
         let point2 = GISGeometricPoint2D(x: -71.060316, y: 49.432044)
         let point3 = GISGeometricPoint2D(x: -72.060316, y: 48.432044)
         let lineString = GISGeometricLineString2D(points: [point, point2, point3, point])
-        let polygon = GISGeometricPolygon2D(exteriorRing: lineString, interiorRings: [])
+        
+        let point4 = GISGeometricPoint2D(x: -71.060316, y: 48.432044)
+        let point5 = GISGeometricPoint2D(x: -71.060316, y: 49.432044)
+        let point6 = GISGeometricPoint2D(x: -72.060316, y: 48.432044)
+        let lineString2 = GISGeometricLineString2D(points: [point4, point5, point6, point4])
+        let polygon = GISGeometricPolygon2D(exteriorRing: lineString, interiorRings: [lineString2, lineString2])
         
         var user = UserArea(id: nil, area: polygon)
         user = try user.save(on: conn).wait()
